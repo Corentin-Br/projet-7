45ce859e35994ad05063bd8ba4e7acc26633f5fb
Corentin BRAVO <corentinbravo@gmail.com>
2021-06-17 14:56:11 +0200

    Initial commit

---
+++ bruteforce.py
@@ -0,0 +1,79 @@
+class Action:
+    def __init__(self, index, cost, benefit):
+        self.index = index
+        self.cost = cost
+        self.benefit = benefit
+
+
+class Wallet:
+    def __init__(self, actions, max_price):
+        self.actions = actions if actions else []
+        self.max_price = max_price
+
+    @property
+    def total_cost(self):
+        return sum([action.cost for action in self.actions])
+
+    @property
+    def gain(self):
+        return sum([action.cost * action.benefit for action in self.actions])
+
+    @property
+    def actions_index(self):
+        return [action.index for action in self.actions]
+
+    def has_place_for(self, action):
+        if self.total_cost + action.cost > self.max_price:
+            return False
+        else:
+            return True
+
+
+def find_valid_wallets(actions, max_price):
+    valid_wallets = [Wallet([], max_price)]
+    for action in actions:
+        new_wallets = []
+        for wallet in valid_wallets:
+            if wallet.has_place_for(action):
+                new_actions = wallet.actions.copy()
+                new_actions.append(action)
+                new_wallets.append(Wallet(new_actions, max_price))
+        valid_wallets += new_wallets
+    return valid_wallets
+
+
+def find_best_wallet(wallets):
+    wallets.sort(key=lambda wallet: wallet.gain, reverse=True)
+    return wallets[0]
+
+
+def main():
+    available_actions = [
+        {"cost": 20, "benefit": 0.05},
+        {"cost": 30, "benefit": 0.1},
+        {"cost": 50, "benefit": 0.15},
+        {"cost": 70, "benefit": 0.2},
+        {"cost": 60, "benefit": 0.17},
+        {"cost": 80, "benefit": 0.25},
+        {"cost": 22, "benefit": 0.07},
+        {"cost": 26, "benefit": 0.11},
+        {"cost": 48, "benefit": 0.13},
+        {"cost": 34, "benefit": 0.27},
+        {"cost": 42, "benefit": 0.17},
+        {"cost": 110, "benefit": 0.09},
+        {"cost": 38, "benefit": 0.23},
+        {"cost": 14, "benefit": 0.01},
+        {"cost": 18, "benefit": 0.03},
+        {"cost": 8, "benefit": 0.08},
+        {"cost": 4, "benefit": 0.12},
+        {"cost": 10, "benefit": 0.14},
+        {"cost": 24, "benefit": 0.21},
+        {"cost": 114, "benefit": 0.18}
+    ]
+    actions = [Action(i, action["cost"], action["benefit"]) for (i, action) in enumerate(available_actions)]
+    best_wallet = find_best_wallet(find_valid_wallets(actions, 500))
+    print(best_wallet.actions_index, best_wallet.total_cost, round(best_wallet.gain, 2))
+
+
+if __name__ == "__main__":
+    main()
---
+++ optimized.py
@@ -0,0 +1,130 @@
+import csv
+from time import time
+
+import numpy as np
+
+MAX_COST = 500
+
+
+def read_csv_file(file_name):
+    """Get the data from a csv file and return it as a list of dictionaries."""
+    with open(f"{file_name}.csv", newline='') as file:
+        csv_data = csv.reader(file)
+        data = []
+        for i, row in enumerate(csv_data):
+            if i == 0:
+                continue
+            data.append({"name": row[0],
+                         "cost": float(row[1]),
+                         "profit": float(row[2])
+                         })
+    return data
+
+
+def clean_data(data):
+    """Return entries in data that can be used."""
+    return [entry for entry in data if is_valid(entry)]
+
+
+def is_valid(entry):
+    """Check if the profit and the cost of the entry are valid."""
+    if MAX_COST < entry["cost"] or entry["cost"] <= 0 or entry["profit"] <= 0:
+        return False
+    return True
+
+
+def increase_scale(data, max_cost):
+    """Increase the order of magnitude of costs.
+
+    The purpose of this function is to prepare for dynamic programming, by transforming all decimal values into
+    integers."""
+    for entry in data:
+        entry["cost"] *= 10
+    max_cost *= 10
+    return data, max_cost
+
+
+def solve_knapsack(data):
+    """Use dynamic programming to solve the knapsack problem."""
+    oom_increase = 1  # Represent the increase in order of magnitude.
+    max_cost = MAX_COST
+    for entry in data:
+        while not format(entry["cost"], '.12g').isnumeric():
+            data, max_cost = increase_scale(data, max_cost)
+            oom_increase *= 10
+        entry["cost"] = round(entry["cost"])
+    solution_array = generate_solving_array(data, max_cost, oom_increase)
+    solution_index, optimal_value, optimal_spending = get_solution_from_array(solution_array, data, oom_increase)
+    solution = ", ".join([data[index]["name"] for index in solution_index])
+    print(f"By buying {solution},")
+    print(f"You'll spend {optimal_spending/oom_increase} and earn {round(optimal_value, 2)}")
+
+
+def solve_knapscak_greedy(data):
+    data.sort(key=lambda element: element["profit"], reverse=True)
+    current_cost = 0
+    solution = []
+    for entry in data:
+        if entry["cost"] + current_cost < 500:
+            solution.append(entry)
+    solution.sort(key=lambda element: element["profit"])
+    return solution
+
+
+def generate_solving_array(data, max_cost, magnitude_increase):
+    """
+    Use dynamic programming to generate an array that contains the optimal value and can be used to determine how to
+    reach said value."""
+    row_number = len(data) + 1
+    column_number = max_cost + 1
+    array = np.empty((row_number, column_number))
+    array[0] = np.zeros(column_number)
+    for i in range(1, len(array)):
+        cost = data[i - 1]["cost"]
+        profit = int(data[i - 1]["cost"] * data[i - 1]["profit"] / magnitude_increase) / 100
+        for j in range(column_number):
+            if j >= cost:
+                array[i, j] = max(array[i - 1, j],
+                                  array[i - 1, j - cost] + profit)
+            else:
+                array[i, j] = array[i - 1, j]
+    return array
+
+
+def get_solution_from_array(array, data, magnitude_increase):
+    """Get the combination of actions providing the optimal value."""
+    solution_index = []
+    optimal_value = array[-1][-1]
+    fitting_values = np.asarray(array == optimal_value).nonzero()  # Some kind of magical formula. asarray
+    # gives an array of the same dimension as the input, but with True or False, depending if they fulfill the
+    # condition or not,  then nonzero() removes all values equal to 0 (here, False).
+    initial_row_index = fitting_values[0][0]
+    column_index = optimal_spending = fitting_values[1][0]
+    for i in range(initial_row_index, -1, -1):
+        cost = data[i - 1]["cost"]
+        profit = int(data[i - 1]["cost"] * data[i - 1]["profit"] / magnitude_increase) / 100
+        if column_index >= cost:
+            if array[i - 1, column_index - cost] + profit >= array[i - 1, column_index]:
+                solution_index.append(i - 1)
+                column_index -= cost
+    return solution_index, optimal_value, optimal_spending
+
+
+def main():
+    keep_going = True
+    while keep_going:
+        file_name = input("What is the name of the csv file containing the data (do not include the extension)?")
+        beginning = time()
+        data = clean_data(read_csv_file(file_name))
+        solve_knapsack(data)
+        end = time()
+        print(beginning - end)
+        more = input("Do you want to solve another set of data? (y/n)")
+        if more != "y":
+            keep_going = False
+            print("The program is closing")
+    return
+
+
+if __name__ == "__main__":
+    main()
